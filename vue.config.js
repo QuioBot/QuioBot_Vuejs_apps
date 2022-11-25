55c2015176a35fbbb073cf6d90225f170fc038ca
elghibaomar <elghibaomar@gmail.com>
2022-11-25 22:47:01 +0100

    l4

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+let publicPath = process.env.NODE_ENV === 'production' ? '/' : '/';
+
+module.exports = {
+  publicPath,
+  productionSourceMap: false,
+};
+
